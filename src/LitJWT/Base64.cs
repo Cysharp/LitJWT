2c233da589a021e02f64dc0f38f5eae968ca63b7
neuecc <ils@neue.cc>
2019-05-23 11:32:01 +0900

    Init codes

---
+++ src/LitJWT/Base64.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace LitJWT
+{
+    internal static unsafe class Base64
+    {
+        static readonly char[] base64EncodeTable = {
+            'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P',
+            'Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f',
+            'g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v',
+            'w','x','y','z','0','1','2','3','4','5','6','7','8','9','+','/'};
+
+        static readonly char[] base64UrlEncodeTable = {
+            'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P',
+            'Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f',
+            'g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v',
+            'w','x','y','z','0','1','2','3','4','5','6','7','8','9','-','_'}; // 62nd '+' => '-', 63rd '/' => '_'
+
+        // String.Join(", ", Enumerable.Range(0, byte.MaxValue).Select(x => Base64.base64EncodeTable.Cast<char?>().FirstOrDefault(y => x == y) ?? -1))
+        static readonly sbyte[] base64DecodeTable = new sbyte[] { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 43, -1, -1, -1, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, -1, -1, -1, -1, -1, -1, -1, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, -1, -1, -1, -1, -1, -1, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 };
+        static readonly sbyte[] base64UrlDecodeTable = new sbyte[] { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 45, -1, -1, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, -1, -1, -1, -1, -1, -1, -1, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, -1, -1, -1, -1, 95, -1, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 };
+
+        public static int GetMaxBase64Length(int length)
+        {
+            return (((length + 2) / 3) * 4);
+        }
+
+        public static int GetMaxBase64UrlLength(int length)
+        {
+            return (((length) / 3) * 4);
+        }
+
+        public static bool TryToBase64Chars(ReadOnlySpan<byte> bytes, Span<char> chars, out int charsWritten)
+        {
+            fixed (byte* inData = &MemoryMarshal.GetReference(bytes))
+            fixed (char* outChars = &MemoryMarshal.GetReference(chars))
+            {
+                charsWritten = ConvertBase64Core(inData, outChars, 0, bytes.Length);
+                return true;
+            }
+        }
+
+        static int ConvertBase64Core(byte* bytes, char* chars, int offset, int length)
+        {
+            var mod3 = length % 3;
+            var loopLength = offset + (length - mod3);
+
+            var i = 0;
+            var j = 0;
+
+            // use pointer to avoid range check
+            fixed (char* table = &base64EncodeTable[0])
+            {
+                for (i = offset; i < loopLength; i += 3)
+                {
+                    // 6(2)
+                    // 2 + 4(4)
+                    // 4 + 2(6)
+                    // 6
+                    chars[j] = table[(bytes[i] & 0b11111100) >> 2];
+                    chars[j + 1] = table[((bytes[i] & 0b00000011) << 4) | ((bytes[i + 1] & 0b11110000) >> 4)];
+                    chars[j + 2] = table[((bytes[i + 1] & 0b00001111) << 2) | ((bytes[i + 2] & 0b11000000) >> 6)];
+                    chars[j + 3] = table[(bytes[i + 2] & 0b00111111)];
+                    j += 4;
+                }
+
+                i = loopLength;
+
+                if (mod3 == 1)
+                {
+                    chars[j] = table[(bytes[i] & 0b11111100) >> 2];
+                    chars[j + 1] = table[((bytes[i] & 0b00000011) << 4) | ((bytes[i + 1] & 0b11110000) >> 4)];
+                    chars[j + 2] = table[((bytes[i + 1] & 0b00001111) << 2)];
+                    chars[j + 3] = '='; // padding
+                    j += 4;
+                }
+                else if (mod3 == 2)
+                {
+                    chars[j] = table[(bytes[i] & 0b11111100) >> 2];
+                    chars[j + 1] = table[((bytes[i] & 0b00000011) << 4)];
+                    chars[j + 2] = '=';
+                    chars[j + 3] = '=';
+                    j += 4;
+                }
+
+                return j;
+            }
+        }
+
+        static int ConvertBase64UrlCore(byte* bytes, char* chars, int offset, int length)
+        {
+            var mod3 = length % 3;
+            var loopLength = offset + (length - mod3);
+
+            var i = 0;
+            var j = 0;
+
+            fixed (char* table = &base64EncodeTable[0])
+            {
+                for (i = offset; i < loopLength; i += 3)
+                {
+                    chars[j] = table[(bytes[i] & 0b11111100) >> 2];
+                    chars[j + 1] = table[((bytes[i] & 0b00000011) << 4) | ((bytes[i + 1] & 0b11110000) >> 4)];
+                    chars[j + 2] = table[((bytes[i + 1] & 0b00001111) << 2) | ((bytes[i + 2] & 0b11000000) >> 6)];
+                    chars[j + 3] = table[(bytes[i + 2] & 0b00111111)];
+                    j += 4;
+                }
+
+                i = loopLength;
+
+                // no-pading '='.
+                if (mod3 == 1)
+                {
+                    chars[j] = table[(bytes[i] & 0b11111100) >> 2];
+                    chars[j + 1] = table[((bytes[i] & 0b00000011) << 4) | ((bytes[i + 1] & 0b11110000) >> 4)];
+                    chars[j + 2] = table[((bytes[i + 1] & 0b00001111) << 2)];
+                    j += 3;
+                }
+                else if (mod3 == 2)
+                {
+                    chars[j] = table[(bytes[i] & 0b11111100) >> 2];
+                    chars[j + 1] = table[((bytes[i] & 0b00000011) << 4)];
+                    j += 2;
+                }
+
+                return j;
+            }
+        }
+    }
+}
---
+++ src/LitJWT/Program.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Buffers;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+
+namespace LitJWT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+
+
+    public static class Jwt
+    {
+        public static void Encode()
+        {
+        }
+    }
+
+
+    public class DefaultDecoder
+    {
+        (StringSegment header, StringSegment payload, StringSegment signature) Split(string text)
+        {
+            StringSegment header = default;
+            StringSegment payload = default;
+            StringSegment signature = default;
+
+            var foundHeader = false;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == '.')
+                {
+                    if (!foundHeader)
+                    {
+                        header = new StringSegment(text, 0, i);
+                        foundHeader = true;
+                    }
+                    else
+                    {
+                        var offset = header.Count + 1;
+                        payload = new StringSegment(text, offset, i - offset);
+                        signature = new StringSegment(text, i + 1, text.Length - (i + 1));
+                        break;
+                    }
+                }
+            }
+
+            return (header, payload, signature);
+        }
+
+        public delegate T PayloadParser<T>(ReadOnlySpan<char> payload);
+
+
+        public void Encode<T>(IBufferWriter<byte> bufferWriter, T payload, IJwtAlgorithm algorithm)
+        {
+            bufferWriter.Write(algorithm.HeaderBase64WithDot);
+            // write payload.
+
+            // algorithm.
+
+
+
+            // bufferWriter.Write(".");
+
+
+
+            // IJwtAlgorithmResolver foo;
+
+            // new Utf8JsonWriter(
+
+        }
+
+
+
+        // return DecodeResult
+        public bool TryDecode<T>(string token, PayloadParser<T> payloadParser, out T payloadResult)
+        {
+            // HttpContext ctx = null;
+            // ctx.Request.
+            // ctx.he
+
+
+            var (header, payload, signature) = Split(token);
+
+            // TryFromBase64Chars
+            Span<byte> bytes = stackalloc byte[header.Count];
+            Convert.TryFromBase64Chars(header.AsSpan(), bytes, out var bytesWritten);
+
+            var headerString = Encoding.UTF8.GetString(bytes.Slice(0, bytesWritten));
+
+            var reader = new Utf8JsonReader(bytes.Slice(0, bytesWritten), true, default(JsonReaderState));
+
+            reader.Read();
+            if (reader.TokenType != JsonTokenType.StartObject)
+            {
+                // invalid
+            }
+
+            while (reader.Read())
+            {
+                switch (reader.TokenType)
+                {
+                    case JsonTokenType.EndObject:
+                        break;
+                    case JsonTokenType.PropertyName:
+                        if (reader.TextEquals("alg"))
+                        {
+                            // try to read algorithm span.
+                            if (reader.Read())
+                            {
+                                var algorithm = reader.ValueSpan;
+                                // select algorithm
+                            }
+                        }
+                        else
+                        {
+                            reader.Read(); // maybe "typ:JWT" but not check.
+                        }
+                        break;
+                    default:
+                        throw new Exception();
+                }
+            }
+
+            // new Utf8JsonWriter(
+
+            // TODO:get expiry or etc.
+            payloadResult = payloadParser(payload.AsSpan());
+
+
+            // verify!
+            return true;
+        }
+
+
+    }
+
+    public interface IJwtAlgorithmResolver
+    {
+        IJwtAlgorithm Resolve(ReadOnlySpan<byte> name);
+    }
+
+    public class UnsafeNoneAlgorithm
+    {
+        byte[] name = Encoding.UTF8.GetBytes("Name");
+        public ReadOnlySpan<byte> Name => name;
+
+
+    }
+
+
+
+    public readonly struct StringSegment
+    {
+        public readonly string Text;
+        public readonly int Offset;
+        public readonly int Count;
+
+        public StringSegment(string text, int offset, int count)
+        {
+            Text = text;
+            Offset = offset;
+            Count = count;
+        }
+
+        public ReadOnlySpan<char> AsSpan()
+        {
+            return Text.AsSpan(Offset, Count);
+        }
+
+        public override string ToString()
+        {
+            return Text.Substring(Offset, Count);
+        }
+    }
+
+
+    public interface IJwtEncoder
+    {
+
+    }
+
+
+    public interface IJwtDecoder
+    {
+
+    }
+
+    public interface IJwtAlgorithm
+    {
+        ReadOnlySpan<byte> Header { get; }
+        ReadOnlySpan<byte> HeaderBase64WithDot { get; }
+    }
+
+
+
+
+    public sealed class HMACSHA256Algorithm : IJwtAlgorithm
+    {
+        static readonly byte[] header = Encoding.UTF8.GetBytes(@"{""alg"":""HS256"",""typ"":""JWT""}");
+        static readonly byte[] headerBase64WithDot = Encoding.UTF8.GetBytes(Convert.ToBase64String(header) + ".");
+
+        ReadOnlySpan<byte> Header => header;
+
+        ReadOnlySpan<byte> IJwtAlgorithm.Header => throw new NotImplementedException();
+
+        ReadOnlySpan<byte> HeaderBase64WithDot => headerBase64WithDot;
+
+        ReadOnlySpan<byte> IJwtAlgorithm.HeaderBase64WithDot => throw new NotImplementedException();
+
+        public byte[] Sign(byte[] key, ReadOnlySpan<byte> source, IBufferWriter<byte> writer)
+        {
+            using (var sha = new HMACSHA256(key))
+            {
+                Span<byte> dest = stackalloc byte[32];
+
+                if (sha.TryComputeHash(source, dest, out var bytesWritten) && bytesWritten == 32)
+                {
+                    // writer.Write(
+                }
+                else
+                {
+                    throw new InvalidOperationException("Can not compute hash.");
+                }
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+
+    public struct JwtHeader
+    {
+
+    }
+
+    public struct JwtPayload
+    {
+
+    }
+
+
+    internal class ReservedConstants
+    {
+        public const string Issuer = "iss";
+        public const string Subject = "sub";
+        public const string Audience = "aud";
+        public const string Expiration = "exp";
+        public const string NotBefore = "nbf";
+        public const string IssuedAt = "iat";
+        public const string JwtIdentifier = "jti";
+    }
+}
